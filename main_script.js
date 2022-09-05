0988ba0730e109ee30d2be0f870580ac25a7f227
Walter Kutschker <walterk@protonmail.com>
2022-09-05 06:05:10 -0300

    1era iteracion maqueta bootstrap

---
+++ main_script.js
@@ -0,0 +1,13 @@
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+
+if (toastTrigger) {
+    toastTrigger.addEventListener('click', () => {
+    const toast = new bootstrap.Toast(toastLiveExample)
+    toast.show()
+    })
+    }
