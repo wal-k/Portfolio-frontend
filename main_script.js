c8666a8099c5acde3508a151f076e9bb7ea04cf9
Walter Kutschker <walterk@protonmail.com>
2022-09-08 01:31:35 -0300

    maquetacion estatica: secciones + placeholders

--- main_script.js
+++ main_script.js
@@ -11,3 +11,13 @@ if (toastTrigger) {
     toast.show()
     })
     }
+
+
+
+const div = document.getElementById("body");
+    
+const paragraph = document.createElement("p");
+    
+paragraph.textContent = 'Hello. I was created dynamically';
+    
+div.appendChild(paragraph);
