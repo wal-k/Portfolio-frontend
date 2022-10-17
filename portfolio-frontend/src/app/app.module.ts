07c9a65cb3200a7d81ad6d09b9e1ae6ee173638b
Walter Kutschker <walterk@protonmail.com>
2022-10-17 03:59:17 -0300

    fixed bootstrap layout

--- portfolio-frontend/src/app/app.module.ts
+++ portfolio-frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -28,6 +29,7 @@ import { ProyectosComponent } from './components/proyectos/proyectos.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
--- portfolio-frontend/src/app/components/header/header.component.ts
+++ portfolio-frontend/src/app/components/header/header.component.ts
@@ -1,9 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  // ...
+} from '@angular/animations';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+  animations: [
+    // animation triggers go here
+  ]
 })
 export class HeaderComponent implements OnInit {
 
